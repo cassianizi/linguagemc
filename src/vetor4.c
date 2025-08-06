0d807183081d733002650d6c7025ea4b9589eb40
Cassiane <cassianizidoro@gmail.com>
2025-08-06 14:56:08 +0000

    exercicio vetor

---
+++ src/vetor4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    system("cleare");
+    int valor;
+    int qtd=0;
+    int i;
+    int vetor[qtd];
+    printf("digite um valor\n");
+    scanf("%d" ,&valor);
+    while(valor != 99){
+        vetor[qtd] = valor;
+        qtd++;
+    printf("digite outro valor\n");
+    scanf("%d" ,&valor);
+    
+    }
+}
