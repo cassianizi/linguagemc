07bd292ed4c2ca30bd4d740abcc37bf1e958f2cf
Cassiane <cassianizidoro@gmail.com>
2025-08-06 11:29:50 +0000

    sudo while

---
+++ src/whiletrianguloinvertido.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int linha = 20;
+    int coluna = 1;
+    while(linha >= 1)
+    {
+        while(coluna <= linha)
+        {
+            printf("@  ");
+            coluna++;
+        }
+        coluna = 1; //resetar a variavel coluna
+        printf("\n");
+        linha--;
+    }
+    return 0;
+    
+    }
